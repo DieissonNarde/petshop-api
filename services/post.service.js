3a21f3ab155b9cf6eb438307e385bc47c8f9c8ae
DieissonNarde <dieissonnarde@gmail.com>
2023-03-10 23:05:54 -0400

    feat: add post service

---
+++ services/post.service.js
@@ -0,0 +1,29 @@
+import PostRepository from '../repositories/post.repository.js';
+
+async function createPost(post) {
+  await PostRepository.createPost(post);
+}
+
+async function getPosts() {
+  return await PostRepository.getPost();
+}
+
+async function updatePost(post) {
+  await PostRepository.updatePost(post);
+}
+
+async function deletePost(postId) {
+  await PostRepository.deletePost(postId);
+}
+
+async function createComentario(comentario, postId) {
+  await PostRepository.createComentario(comentario, postId);
+}
+
+export default {
+  createPost,
+  getPosts,
+  deletePost,
+  updatePost,
+  createComentario,
+};
