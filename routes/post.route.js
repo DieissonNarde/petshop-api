80245782d2d6bec0f82a4c80a2607b55c1ba6f5a
DieissonNarde <dieissonnarde@gmail.com>
2023-03-10 23:06:37 -0400

    feat: add post route

---
+++ routes/post.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import PostController from '../controllers/post.controller.js';
+
+const router = express.Router();
+
+router.post('/', PostController.createPost);
+router.get('/', PostController.getPosts);
+router.delete('/:id', PostController.deletePost);
+router.put('/', PostController.updatePost);
+router.post('/comentario', ProductController.createComentario);
+
+export default router;
